df2307eb2261246786fa239248caa59669cb9033
Hatan2003 <hatanali.154@gmail.com>
2022-04-02 22:44:11 +0300

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x36A78596cE7685cf63B744915B1Ef12A8981F1C9"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
