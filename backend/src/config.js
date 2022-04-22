a5a66daed8561bd18da8b26b159752abaec03bc4
Hatan2003 <hatanali.154@gmail.com>
2022-04-22 04:59:29 +0300

    (1)

--- backend/src/config.js
+++ backend/src/config.js
@@ -14,7 +14,7 @@ const baseUri = "ipfs://NewUriToReplace"; // This will be replaced automatically
 // If you have selected Solana then the collection starts from 0 automatically
 const layerConfigurations = [
   {
-    growEditionSizeTo: 25,
+    growEditionSizeTo: 5000,
     layersOrder: [
       { name: "Body Color" },
       { name: "Clothes" },
